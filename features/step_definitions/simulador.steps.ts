c8e333b5761ed6c6dd853d2aa3ac68d0a3e5d607
c.gallegos09 <c.gallegos09@ufromail.cl>
2026-09-07 03:14:51 -0300

    test: [paso no implementado] crea glue code vacio

--- features/step_definitions/simulador.steps.ts
+++ features/step_definitions/simulador.steps.ts
@@ -110,4 +110,6 @@ Then('el sistema no interpreta el empate como un resultado desfavorable', functi
     expect(simulationResult.status).toBe('success');
 });
 
-// Escenario 3 pasando en verde
+Given('la carrera {string} exige puntajes solo en NEM, Ranking, Comp. Lectora y Comp. Matemática', function (string) {
+  return 'pending';
+});
